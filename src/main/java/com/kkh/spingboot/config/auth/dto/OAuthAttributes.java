4dcfe96f59259c09e14879bc0210813a761dff98
kimchijoa <amsmdmfm159@naver.com>
2023-01-05 13:35:43 +0900

    spring security client google oauth2 add ver1.3 add naver oauth

--- src/main/java/com/kkh/spingboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/kkh/spingboot/config/auth/dto/OAuthAttributes.java
@@ -37,6 +37,19 @@ public class OAuthAttributes {
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
     public User toEntity() {
        return User.builder()
                .name(name)
