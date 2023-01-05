bec449ec39310be56714e8d56c556d487ac5fc89
kimchijoa <amsmdmfm159@naver.com>
2023-01-05 15:31:00 +0900

    spring security client google oauth2 add ver1.3 add naver oauth

--- src/test/java/com/kkh/spingboot/SpingBootApplicationTests.java
+++ src/test/java/com/kkh/spingboot/SpingBootApplicationTests.java
@@ -1,13 +1,13 @@
-package com.kkh.spingboot;
-
-import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SpingBootApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.kkh.spingboot;
+//
+//import org.junit.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SpingBootApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
